45e87e830264f343876fbe50b522b4214a21c410
David DeWert <daviddewert@gmail.com>
2026-05-08 19:01:45 -0700

    save the old game app

---
+++ tests/game_app/game_app_main_v0.cpp
@@ -0,0 +1,35 @@
+// tests/game_app/game_app_main.cpp
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#include <engine/game_app.h>
+
+
+static void app_update(
+    wz::engine::Context& ctx,
+    wz::engine::FrameContext& fctx,
+    void* user_data)
+{
+    auto& app = *static_cast<wz::app::GameApp*>(user_data);
+
+    wz::app::update(ctx, fctx, app);
+    wz::app::render(app, fctx);
+}
+
+
+int main()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    wz::app::GameApp app{};
+
+    if (!wz::app::init(app))
+    {
+        wz::app::shutdown(app);
+        return 1;
+    }
+
+    wz::engine::run(app_update, &app);
+
+    wz::app::shutdown(app);
+    return 0;
+}
